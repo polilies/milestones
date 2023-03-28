babfba6680d8a826e8749f1b8514c85e69cea5dc
Alican KAYIKCI <alijan84@hotmail.com>
2023-03-28 12:53:10 +0300

    final state of the project

--- bookstore_docker_compose/terra/hostdata.tpl
+++ bookstore_docker_compose/terra/hostdata.tpl
@@ -5,9 +5,16 @@ service docker start
 systemctl enable docker
 usermod -a -G docker ec2-user
 newgrp docker
+curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
-yum install git -y
-TOKEN= ${mytoken}
-cd /home/ec2-user && git clone https://$TOKEN@github.com/polilies/phonebook.git
+#yum install git -y
+yum install wget
+cd /home/ec2-user/
+mkdir bookstore && cd bookstore
+wget -O Dockerfile https://raw.githubusercontent.com/polilies/bookstore_project/main/Dockerfile
+wget -O docker-compose.yml https://raw.githubusercontent.com/polilies/bookstore_project/main/docker-compose.yml
+#cd vol
+wget -O bookstore-api.py https://raw.githubusercontent.com/polilies/bookstore_project/main/bookstore-api.py
+wget -O requirements.txt https://raw.githubusercontent.com/polilies/bookstore_project/main/requirements.txt
 #python3 /home/ec2-user/phonebook/phonebook-app.py
-docker-compose up -d
+docker-compose --project-name bookstore up
--- bookstore_docker_compose/terra/main.tf
+++ bookstore_docker_compose/terra/main.tf
@@ -2,9 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~>4.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.18.3"
     }
   }
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
 }
 
 # Configure the AWS Provider
@@ -17,13 +25,30 @@ resource "aws_default_vpc" "default" {
 }
 
 data "template_file" "temp" {
-  template = "${file("${path.module}/hostdata.tpl")}"
-  vars = {
-    mytoken = "${var.token}"
-  }
+  template = file("${path.module}/hostdata.tpl")
 }
 
+resource "github_repository" "bookstore" {
+  name        = "bookstore_project"
+  description = "docker-compose, docker build, terraform github repo creation, aws_ec2 "
+  auto_init   = true
+  visibility  = "public"
+  #branch      = "main"
+  /*   template {
+    owner                = "github"
+    repository           = "polilies  "
+    include_all_branches = true
+  } */
+}
 
+resource "github_repository_file" "create" {
+  for_each            = toset(var.paths)
+  content             = file("${var.basepath}/${each.value}")
+  file                = each.value
+  repository          = github_repository.bookstore.name
+  overwrite_on_create = true
+  branch              = "main"
+}
 
 data "aws_ami" "tf_ami" {
   most_recent = true
@@ -36,25 +61,25 @@ data "aws_ami" "tf_ami" {
 }
 
 locals {
-  instance-type = "t2.micro"
-  key-name = "nvirginia"
-  secgr-dynamic-ports = [22,80]
-  user = "poli"
+  instance-type       = "t2.micro"
+  key-name            = "nvirginia"
+  secgr-dynamic-ports = [22, 80]
+  user                = "poli"
 }
 
 resource "aws_security_group" "book-sec-gr" {
-  name        = "${local.user}-docker-sec-gr"
+  name        = "${local.user}-api-sec-gr"
   description = "Allow SSH inbound traffic"
 
   dynamic "ingress" {
     for_each = local.secgr-dynamic-ports
     content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-}
 
   egress {
     description = "Outbound Allowed"
@@ -66,12 +91,12 @@ resource "aws_security_group" "book-sec-gr" {
 }
 
 resource "aws_instance" "tf-ec2" {
-  ami           = data.aws_ami.tf_ami.id
-  instance_type = local.instance-type
-  key_name = local.key-name
-  vpc_security_group_ids = [ aws_security_group.book-sec-gr.id ]
+  ami                    = data.aws_ami.tf_ami.id
+  instance_type          = local.instance-type
+  key_name               = local.key-name
+  vpc_security_group_ids = [aws_security_group.book-sec-gr.id]
   tags = {
-      Name = "bookstore-instance"
+    Name = "bookstore-instance"
   }
   user_data = data.template_file.temp.rendered
-} 
+}
